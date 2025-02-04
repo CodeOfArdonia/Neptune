a40e322cdaf481fde84750439f771071bb1157d9
IAFEnvoy <tanksheng545@126.com>
2025-02-05 00:00:47 +0800

    fix boss bar render crash

--- common/src/main/java/com/iafenvoy/neptune/mixin/BossBarHudMixin.java
+++ common/src/main/java/com/iafenvoy/neptune/mixin/BossBarHudMixin.java
@@ -6,7 +6,9 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.BossBarHud;
 import net.minecraft.entity.boss.BossBar;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,7 +18,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BossBarHudMixin {
     @Inject(method = "renderBossBar(Lnet/minecraft/client/gui/DrawContext;IILnet/minecraft/entity/boss/BossBar;II)V", at = @At("HEAD"), cancellable = true)
     private void onRenderHud(DrawContext context, int x, int y, BossBar bossBar, int width, int height, CallbackInfo ci) {
-        if (BossBarRenderHelper.render(context, x, y, bossBar, width, height))
+        if (neptune$render(context, x, y, bossBar, width, height))
             ci.cancel();
+    }
+
+    @Unique
+    private static boolean neptune$render(DrawContext context, int x, int y, BossBar bossBar, int width, int height) {
+        for (BossBarRenderHelper.BossBarInfo info : BossBarRenderHelper.infos.values())
+            if (info.bossBarId().contains(bossBar.getUuid())) {
+                if (height == 5) {
+                    context.drawTexture(BossBarRenderHelper.BARS_TEXTURE, x, y, 0, bossBar.getColor().ordinal() * 5 * 2 + 5, width, 5);
+                    context.drawTexture(info.texture(), x - 10, y - 7, 0, 0, 0, 206, 19, 206, 19);
+                }
+                if (info.disableName()) bossBar.setName(Text.literal(""));
+                return true;
+            }
+        return false;
     }
 }
